cfe9f9664d17606bae42fdce4941837d1f4083f4
Kelechi Odom <kelechiodom10@gmail.com>
2025-05-06 02:50:50 +0100

    Revert "feat: add multi-tenancy support with organizations, members and invitations models"
    This reverts commit b4a9652164b20318ae80d213fa5413c58d2f36e7.

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { organization } from "better-auth/plugins";
 
 const prisma = new PrismaClient();
 
@@ -23,7 +22,6 @@ export const auth = betterAuth({
           }
         : undefined,
   },
-  plugins: [organization()],
 });
 
 export type AuthType = {
