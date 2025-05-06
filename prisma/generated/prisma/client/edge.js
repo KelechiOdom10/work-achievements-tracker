e904d0af8e1eeb5429d4d536edd6d05a9f7961b5
Kelechi Odom <kelechiodom10@gmail.com>
2025-05-06 11:41:29 +0100

    chore: remove hardcoded database connection string and set postinstall flag to false

--- prisma/generated/prisma/client/edge.js
+++ prisma/generated/prisma/client/edge.js
@@ -275,11 +275,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:leomessi@localhost:5432/achievements"
+        "value": null
       }
     }
   },
