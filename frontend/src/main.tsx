60169f8db919e4c0a8afc3b27feff8e87bc5c809
Kelechi Odom <kelechiodom10@gmail.com>
2025-05-13 18:07:31 +0100

    refactor: remove error boundary, configure eslint rules and add vscode settings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,40 +1,10 @@
-import React, { ErrorInfo, StrictMode } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
 import { App } from "./app";
 import { router } from "./lib/router";
-
-class ErrorBoundary extends React.Component<
-  {
-    children: React.ReactNode;
-  },
-  { hasError: boolean; error: Error | null; errorInfo: ErrorInfo | null }
-> {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false, error: null, errorInfo: null };
-  }
-  static getDerivedStateFromError(error) {
-    return { hasError: true, error, errorInfo: null };
-  }
-  componentDidCatch(error, errorInfo) {
-    this.setState({ error, errorInfo });
-    // Optionally log error to an error reporting service
-  }
-  render() {
-    if (this.state.hasError) {
-      return (
-        <pre style={{ color: "red", whiteSpace: "pre-wrap" }}>
-          {String(this.state.error)}\n
-          {this.state.errorInfo ? this.state.errorInfo.componentStack : ""}
-        </pre>
-      );
-    }
-    return this.props.children;
-  }
-}
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -48,9 +18,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <App />
     </StrictMode>
   );
 }
