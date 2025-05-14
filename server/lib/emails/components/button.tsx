12843edcc98d8786a2cbe30ed2b0f08c8c5d8f61
Kelechi Odom <kelechiodom10@gmail.com>
2025-05-14 19:18:19 +0100

    style: update button styles mate

--- server/lib/emails/components/button.tsx
+++ server/lib/emails/components/button.tsx
@@ -1,10 +1,11 @@
 import { Button, Section, type ButtonProps } from "@react-email/components";
+import React from "react";
 
 export const EmailButton = ({ children, href, ...props }: ButtonProps) => {
   return (
     <Section className="my-6 first:mt-0 last:mb-0">
       <Button
-        className="rounded-md bg-neutral-950 px-5 py-3 text-center font-medium text-sm text-white no-underline"
+        className="inline-flex items-center justify-center rounded-lg bg-neutral-950 h-8 leading-8 px-4 py-1.5 text-center font-medium text-sm text-white no-underline"
         href={href}
         {...props}
       >
